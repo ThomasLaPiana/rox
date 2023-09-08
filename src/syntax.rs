e33506f50adce45fe656fc7ca5941252ebcadf69
Thomas <ThomasLaPiana@users.noreply.github.com>
2023-09-08 22:17:12 +0800

    added support for version requirement parsing

--- src/syntax.rs
+++ src/syntax.rs
@@ -2,6 +2,15 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
+pub struct VersionRequirements {
+    command: String,
+    minimum_version: Option<Vec<String>>,
+    maximum_version: Option<Vec<String>>,
+    versions: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug)]
 pub struct RoxFile {
     rox_version: f64,
+    version_requirements: Vec<VersionRequirements>,
 }
