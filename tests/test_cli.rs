d397d1ce9fe3a129a4e3e374f3d0ebac839a6e4b
Thomas <thomas.lapiana+github@pm.me>
2023-09-18 14:57:06 -0700

    add additional tests

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -1,7 +1,33 @@
 use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
 
 #[test]
-fn test_no_subcmd_fails() {
+fn dies_no_args() {
     let mut cmd = Command::cargo_bin("rox").unwrap();
-    cmd.assert().code(2);
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Usage"));
+}
+
+#[test]
+fn help_succeeds() {
+    let mut cmd = Command::cargo_bin("rox").unwrap();
+    cmd.arg("help")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Usage"));
+}
+
+/// This is a useful example for testing output
+#[test]
+#[ignore]
+fn build_binary_output() {
+    let expected = "tests/expected/help_output.txt";
+    let output = fs::read_to_string(expected).unwrap();
+    let mut cmd = Command::cargo_bin("rox").unwrap();
+    cmd.arg("-s task build-release-binary")
+        .assert()
+        .success()
+        .stdout(output);
 }
